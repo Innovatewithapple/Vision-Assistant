932dc7d781673a9db1d4c2adc68c8fe542f00954
Mihir Vyas <mihirv126@gmail.com>
2026-09-12 16:47:07 +0530

    detection added but still too slow

---
+++ detection.py
@@ -0,0 +1,42 @@
+import cv2
+import torch
+import torchvision
+
+class Detector:
+    def __init__(self):
+        weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+        self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights)
+
+        self.model.eval()
+        self.class_names = weights.meta['categories']
+
+    def detect(self,frame):
+        #----Resize for faster inference-----!
+        original_height, original_width = frame.shape[:2]
+
+        new_width = 800
+        new_height = int(original_height * new_width / original_width)
+
+        resized_frame = cv2.resize(frame,(new_width,new_height))
+
+        #BGR->RGB
+        frame_rgb = cv2.cvtColor(resized_frame,cv2.COLOR_BGR2RGB)
+        image_tensor = torch.from_numpy(frame_rgb).permute(2,0,1).float() / 255.0
+
+        with torch.no_grad():
+            predicton = self.model([image_tensor])[0]
+
+        boxes= predicton['boxes']
+        labels = predicton['labels']
+        scores = predicton['scores']
+
+        #Convert boxes back to original frame coordinates
+        scale_x = original_width / new_width
+        scale_y = original_height / new_height
+
+        boxes[:, 0] *= scale_x
+        boxes[:, 1] *= scale_y
+        boxes[:, 2] *= scale_x
+        boxes[:, 3] *= scale_y
+
+        return boxes, labels, scores, self.class_names 
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import cv2
+import torch
+import torchvision
+from detection import Detector
 
 video_path = '/Users/himanshuvyas/Downloads/vision-assistant/Video/streetwalking.mp4'
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print("Using device:", device)
 
+#---------Capturing Start--------!
 cap = cv2.VideoCapture(video_path) # create video capture object
+detector = Detector()
 
 while True:
     ret,frame = cap.read() # ret is boolean value to get if frame captured or not and frame is getting numpy values of video for each frame
@@ -10,17 +17,96 @@ while True:
     if not ret:
         break
 
-    #Draw Rectangle
-    cv2.rectangle(img=frame,pt1=(100,100),pt2=(400,300),color=(0,255,0),thickness=2)
+    boxes,labels,scores,class_names = detector.detect(frame=frame)
 
-    #Draw Text
-    cv2.putText(img=frame,text="Street Video",org=(100,80),fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,fontScale=1,color=(0,255,0),thickness=2)
+    for box,label,score in zip(boxes,labels,scores):
+        if score < 0.5:
+            continue
 
-    cv2.imshow("Street Video",frame)
+        x1,y1,x2,y2 = box.int().tolist()
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        #Draw Rectangle
+        cv2.rectangle(img=frame,pt1=(x1,y1),pt2=(x2,y2),color=(0,255,0),thickness=2)
+
+        #Here one important note. When we draw a rectangle, that doesnt mean we are putting rectangle on top of frames. We just changes pixels colors to whatever we want our rectangular boder
+        #..so the pixel values changed and show rectangular shape
+
+        #Draw Text
+        class_name = class_names[label.item()]
+        text = f"{class_name}: {score.item():.2f}"
+        cv2.putText(img=frame,text=text,org=(x1,y1-10),fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,fontScale=1,color=(0,255,0),thickness=3)
+
+        cv2.imshow("Street Video",frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
 
+
+#======================================================xxxxxxxxxxxxxxxxxxxxxx====================================================xxxxxxxxxxxxx==================================
+
+# video_path = "/Users/himanshuvyas/Downloads/vision-assistant/Video/streetwalking.mp4"
+
+# cap = cv2.VideoCapture(video_path)
+
+# ret, frame = cap.read()
+
+# if not ret:
+#     print("Could not read video")
+#     cap.release()
+#     exit()
+
+# #-------Load Pretrained Fast R-CNN----------!
+# weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+
+# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=weights)
+
+# model.eval()
+
+
+# #-----Prepare the Frame--------!
+# frame_rgb = cv2.cvtColor(src=frame,code=cv2.COLOR_BGR2RGB)
+# image_tensor = torch.from_numpy(frame_rgb).permute(2,0,1).float() / 255.0
+
+
+# #-----Run Detection---------!
+# with torch.no_grad():
+#     predictions = model([image_tensor])
+
+# prediction = predictions[0]
+
+
+# #-----Inspect the Output-------!
+# boxes = prediction['boxes']
+# print("Boxes:")
+# print(prediction["boxes"])
+
+# labels = prediction['labels']
+# print("\nLabels:")
+# print(prediction["labels"])
+
+# scores = prediction['scores']
+# print("\nScores:")
+# print(prediction["scores"])
+
+# #----------Draw boxes and put labels with score-------------!
+# for box,label,score in zip(boxes,labels,scores):
+#     if score < 0.5:
+#         break
+
+#     x1,y1,x2,y2 = box.int().tolist()
+
+#     cv2.rectangle(img=frame,pt1=(x1,y1),pt2=(x2,y2),color=(0,255,0),thickness=2)
+
+#     text = f"Class {label.item()} : {score.item():.2f}"
+
+#     cv2.putText(img=frame,text=text,org=(x1,y1-10),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.8,color=(0,255,0),thickness=2)
+
+
+# #-----Show Result-----!
+# cv2.imshow("Street Video",frame)
+# cv2.waitKey(0)
+# cap.release()
+# cv2.destroyAllWindows()
